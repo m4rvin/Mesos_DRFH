cbd8883996d3c1af48b8a8edb51664521b7f913f
Daniele Angelucci <angelucci.dnl@gmail.com>
2017-03-01 13:44:42 +0100

    Bugfix in cluster handling script.
    Changed pathname for clusterNodesPids file.
    Removed echoing of pid for frameworks processes.

--- ClusterHandling/localTestbedShell_frameworks.sh
+++ ClusterHandling/localTestbedShell_frameworks.sh
@@ -6,7 +6,7 @@ MESOS_MAIN_DIR="/home/m4rvin/mesos_drfh/"
 MESOS_EXECUTABLES_PATH="$MESOS_MAIN_DIR/build/bin"
 MESOS_FRAMEWORK_EXECUTABLES_PATH="$MESOS_MAIN_DIR/build/src"
 CLUSTER_NODES_PIDS_FILENAME="clusterNodesPids"
-CLUSTER_NODES_PIDS_FILEPATH="$MESOS_MAIN_DIR/$CLUSTER_NODES_PIDS_FILENAME"
+CLUSTER_NODES_PIDS_FILEPATH="$MESOS_MAIN_DIR/ClusterHandling/$CLUSTER_NODES_PIDS_FILENAME"
 
 
 ###########################
@@ -17,4 +17,4 @@ cd $MESOS_FRAMEWORK_EXECUTABLES_PATH
 ./test-framework-drfh --master=127.0.0.1:5050 --task_cpus_demand=1.1 --task_memory_demand=64MB --offers_limit=200 &
 pid=$!
 echo $pid
-echo $pid >> "$CLUSTER_NODES_PIDS_FILEPATH"
+#echo $pid >> "$CLUSTER_NODES_PIDS_FILEPATH"
