12a84238a066167c21c57a80c87573bffe294881
Daniele Angelucci <angelucci.dnl@gmail.com>
2017-04-05 20:10:22 +0200

    Added confB , so an option to select it.

--- ClusterHandling/localTestbedShell_frameworks.sh
+++ ClusterHandling/localTestbedShell_frameworks.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-if [ "$#" -lt 3 ]; then
+if [ "$#" -lt 4 ]; then
 	echo ">>> WARNING: You must specify the duration (in secs) for the frameworks."
 	echo ">>> WARNING: You must specify the workload distribution (LogNorm,Exp) for the frameworks'workloads."
 	echo ">>> WARNING: You must specify the desired cluster Utilization (cpu bound), e.g. 75,85,...,100."
+	echo ">>> WARNING: You must specify the desired configuration of the workload: confA, confB (i.e. DRFbypass, or GoogleTrace-like)."
 	exit
 fi
 
@@ -32,22 +33,46 @@ if [ "$2" == "LogNorm" ]; then #->> start LogNorm section
 
 	if [ "$3" -eq "100" ]; then
 		######### LogNorm U=100%
-		## Conf__DRF-bypass
-		time ./test-framework-drfh --master=127.0.0.1:5050 --task_duration=10 --task_cpus_demand=0.1 --task_memory_demand=128MB --duration=$fwDuration --offers_stats_file="$FRAMEWORK_STATS_FOLDER/framework-1.dat " --generators_seed="framework-low" --interarrivals_distribution="LogNormal,-5.33993575760899,0.5" &
-		pid=$!
-		echo $pid
+		if [ "$4" == "confA" ]; then
+			## Conf__DRF-bypass
+			time ./test-framework-drfh --master=127.0.0.1:5050 --task_duration=10 --task_cpus_demand=0.1 --task_memory_demand=128MB --duration=$fwDuration --offers_stats_file="$FRAMEWORK_STATS_FOLDER/framework-1.dat " --generators_seed="framework-low" --interarrivals_distribution="LogNormal,-5.33993575760899,0.5" &
+			pid=$!
+			echo $pid
 
-		time ./test-framework-drfh --master=127.0.0.1:5050 --task_duration=10 --task_cpus_demand=1 --task_memory_demand=1024MB --duration=$fwDuration --offers_stats_file="$FRAMEWORK_STATS_FOLDER/framework-2.dat " --generators_seed="framework-common" --interarrivals_distribution="LogNormal,-2.34420348405499,0.5" &
-		pid=$!
-		echo $pid
+			time ./test-framework-drfh --master=127.0.0.1:5050 --task_duration=10 --task_cpus_demand=1 --task_memory_demand=1024MB --duration=$fwDuration --offers_stats_file="$FRAMEWORK_STATS_FOLDER/framework-2.dat " --generators_seed="framework-common" --interarrivals_distribution="LogNormal,-2.34420348405499,0.5" &
+			pid=$!
+			echo $pid
 
-		time ./test-framework-drfh --master=127.0.0.1:5050 --task_duration=10 --task_cpus_demand=1 --task_memory_demand=4096MB --duration=$fwDuration --offers_stats_file="$FRAMEWORK_STATS_FOLDER/framework-3.dat " --generators_seed="framework-memint" --interarrivals_distribution="LogNormal,-1.93328877117927,0.5" &
-		pid=$!
-		echo $pid
+			time ./test-framework-drfh --master=127.0.0.1:5050 --task_duration=10 --task_cpus_demand=1 --task_memory_demand=4096MB --duration=$fwDuration --offers_stats_file="$FRAMEWORK_STATS_FOLDER/framework-3.dat " --generators_seed="framework-memint" --interarrivals_distribution="LogNormal,-1.93328877117927,0.5" &
+			pid=$!
+			echo $pid
 
-		time ./test-framework-drfh --master=127.0.0.1:5050 --task_duration=10 --task_cpus_demand=4 --task_memory_demand=256MB --duration=$fwDuration --offers_stats_file="$FRAMEWORK_STATS_FOLDER/framework-4.dat " --generators_seed="framework-cpuint" --interarrivals_distribution="LogNormal,-0.957909122935104,0.5" &
-		pid=$!
-		echo $pid
+			time ./test-framework-drfh --master=127.0.0.1:5050 --task_duration=10 --task_cpus_demand=4 --task_memory_demand=256MB --duration=$fwDuration --offers_stats_file="$FRAMEWORK_STATS_FOLDER/framework-4.dat " --generators_seed="framework-cpuint" --interarrivals_distribution="LogNormal,-0.957909122935104,0.5" &
+			pid=$!
+			echo $pid
+
+		elif [ "$4" == "confB" ]; then
+			## Conf__GoogleTrace-like
+			time ./test-framework-drfh --master=127.0.0.1:5050 --task_duration=10 --task_cpus_demand=0.1 --task_memory_demand=128MB --duration=$fwDuration --offers_stats_file="$FRAMEWORK_STATS_FOLDER/framework-1.dat " --generators_seed="framework-low" --interarrivals_distribution="LogNormal,-4.42364502573483,0.5" &
+			pid=$!
+			echo $pid
+
+			time ./test-framework-drfh --master=127.0.0.1:5050 --task_duration=10 --task_cpus_demand=1 --task_memory_demand=1024MB --duration=$fwDuration --offers_stats_file="$FRAMEWORK_STATS_FOLDER/framework-2.dat " --generators_seed="framework-common" --interarrivals_distribution="LogNormal,-2.92836038090654,0.5" &
+			pid=$!
+			echo $pid
+
+			time ./test-framework-drfh --master=127.0.0.1:5050 --task_duration=10 --task_cpus_demand=1 --task_memory_demand=4096MB --duration=$fwDuration --offers_stats_file="$FRAMEWORK_STATS_FOLDER/framework-3.dat " --generators_seed="framework-memint" --interarrivals_distribution="LogNormal,-1.82974809223843,0.5" &
+			pid=$!
+			echo $pid
+
+			time ./test-framework-drfh --master=127.0.0.1:5050 --task_duration=10 --task_cpus_demand=4 --task_memory_demand=256MB --duration=$fwDuration --offers_stats_file="$FRAMEWORK_STATS_FOLDER/framework-4.dat " --generators_seed="framework-cpuint" --interarrivals_distribution="LogNormal,-0.818147180559945,0.5" &
+			pid=$!
+			echo $pid
+
+		else
+			echo "Not recognized Configuration value."
+			exit
+		fi
 
 	elif [ "$3" -eq "95" ]; then
 
